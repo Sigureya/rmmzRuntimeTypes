29a2d32407ffa26a5ba30f87a599261bc26b0cfa
Sigureya <sigureya1208@gmail.com>
2025-01-15 01:41:45 +0900

    anyの箇所を修正

--- src/object/Game_CommonEvent.ts
+++ src/object/Game_CommonEvent.ts
@@ -1,3 +1,4 @@
+import type { Data_CommonEvent, EventCommand } from "@sigureya/rpgtypes";
 import type { Game_Interpreter } from "./Game_Interpreter";
 
 export declare class Game_CommonEvent {
@@ -5,8 +6,8 @@ export declare class Game_CommonEvent {
   _interpreter: Game_Interpreter | null;
   constructor(commonEventId: number);
   initialize(commonEventId: number): void;
-  event(): any;
-  list(): any[];
+  event(): Data_CommonEvent | undefined;
+  list(): EventCommand[];
   refresh(): void;
   isActive(): boolean;
   update(): void;
