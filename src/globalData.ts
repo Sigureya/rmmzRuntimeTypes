c8473b825752cd5f786c84ea42704ae132f57855
Sigureya <sigureya1208@gmail.com>
2025-01-15 03:26:44 +0900

    $data**を扱う変数を追加

---
+++ src/globalData.ts
@@ -0,0 +1,30 @@
+import type {
+  Data_Actor,
+  Data_Animation,
+  Data_Armor,
+  Data_Class,
+  Data_CommonEvent,
+  Data_Enemy,
+  Data_Item,
+  Data_MapInfo,
+  Data_Skill,
+  Data_State,
+  Data_System,
+  Data_Tileset,
+  Data_Weapon,
+  IdentifiedItems,
+} from "@sigureya/rpgtypes";
+
+export declare let $dataActors: IdentifiedItems<Data_Actor>;
+export declare let $dataArmors: IdentifiedItems<Data_Armor>;
+export declare let $dataAnimations: IdentifiedItems<Data_Animation>;
+export declare let $dataClasses: IdentifiedItems<Data_Class>;
+export declare let $dataCommonEvents: IdentifiedItems<Data_CommonEvent>;
+export declare let $dataEnemies: IdentifiedItems<Data_Enemy>;
+export declare let $dataItems: IdentifiedItems<Data_Item>;
+export declare let $dataStates: IdentifiedItems<Data_State>;
+export declare let $dataSkills: IdentifiedItems<Data_Skill>;
+export declare let $dataSystem: Data_System;
+export declare let $dataTilesets: IdentifiedItems<Data_Tileset>;
+export declare let $dataMapinfos: IdentifiedItems<Data_MapInfo>;
+export declare let $dataWeapons: IdentifiedItems<Data_Weapon>;
