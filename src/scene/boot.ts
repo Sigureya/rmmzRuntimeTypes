b24be90a41207891df7f1affc5d2bd6d965c4f63
Sigureya <sigureya1208@gmail.com>
2025-09-03 01:25:02 +0900

    起動直後に呼ばれるscene。一部のメソッドだけ定義しておけばOK

--- src/scene/boot.ts
+++ src/scene/boot.ts
@@ -1,3 +1,16 @@
 import { Scene_Base } from "./sceneBase";
 
-export declare class Scene_Boot extends Scene_Base {}
+export declare class Scene_Boot extends Scene_Base {
+  create(): void;
+  isReady(): boolean;
+  start(): void;
+  onDatabaseLoaded(): void;
+  setEncryptionInfo(): void;
+  loadSystemImages(): void;
+  loadPlayerData(): void;
+  loadGameFonts(): void;
+  startNormalGame(): void;
+  resizeScreen(): void;
+  updateDocumentTitle(): void;
+  checkPlayerLocation(): void;
+}
--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,2 +1,7 @@
-export * from "./sceneBase";
-export * from "./SceneMenuBase";
+export type { Scene_Base } from "./sceneBase";
+export type { Scene_MenuBase } from "./SceneMenuBase";
+export type { Scene_Boot } from "./boot";
+export type { Scene_File } from "./fIle";
+export type { Scene_Load } from "./fileLoad";
+export type { Scene_Save } from "./fileSave";
+export type { Scene_ItemBase } from "./itemBase";
